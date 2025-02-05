301d7c6f582d3cc68e152d37ae17c26d8d2cbdf9
raphael <raphaelhery42@gmail.com>
2025-02-05 13:45:48 +0100

    branding cvar

--- src/client/component/branding.cpp
+++ src/client/component/branding.cpp
@@ -7,26 +7,33 @@
 
 namespace branding
 {
+	game::cvar_t* branding;
+	
 	class component final : public component_interface
 	{
 	public:
 		void post_unpack() override
 		{
+			branding = game::Cvar_Get("branding", "1", CVAR_ARCHIVE);
+			
 			scheduler::loop([]()
 			{
-				const auto x = 1;
-				const auto y = 12;
-				const auto fontID = 1;
-				const auto scale = 0.25f;
-				float color[4] = { 1.f, 1.f, 1.f, 0.80f };
-				float color_shadow[4] = { 0.f, 0.f, 0.f, 0.80f };
-				const auto* text = MOD_NAME;
+					if (!branding->integer)
+						return;
+					
+					const auto x = 1;
+					const auto y = 12;
+					const auto fontID = 1;
+					const auto scale = 0.25f;
+					float color[4] = { 1.f, 1.f, 1.f, 0.80f };
+					float color_shadow[4] = { 0.f, 0.f, 0.f, 0.80f };
+					const auto* text = MOD_NAME;
 
-				// Draw a drop shadow first				
-				game::SCR_DrawString(x + 1, y + 1, fontID, scale, color_shadow, text, NULL, NULL, NULL);				
-				game::SCR_DrawString(x, y, fontID, scale, color, text, NULL, NULL, NULL);
+					// Draw a drop shadow first				
+					game::SCR_DrawString(x + 1, y + 1, fontID, scale, color_shadow, text, NULL, NULL, NULL);				
+					game::SCR_DrawString(x, y, fontID, scale, color, text, NULL, NULL, NULL);
 
-			}, scheduler::pipeline::renderer);
+			}, scheduler::renderer);
 		}
 	};
 }
